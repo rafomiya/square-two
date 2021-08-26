94c458059da2fca58167b53ba70abc56f4acbf25
rafomiya <omiyarafael@gmail.com>
2021-08-26 18:04:39 -0300

    AWS S3 connection

--- web/models/SQLViews.php
+++ web/models/SQLViews.php
@@ -1,4 +1,4 @@
 <?php
 $views = array(
-    "get_products" => "SELECT id_prod as `id`, name_cat as `category`, name_brand as `brand`, model_prod as `model`, preco_prod as `price`, descr_prod as `description` from product inner join brand on product.id_brand = brand.id_brand inner join category on product.id_category = category.id_cat;"
+    "get_products" => "SELECT id_prod as `id`, name_cat as `category`, name_brand as `brand`, model_prod as `model`, preco_prod as `price`  , image_name as `image` from product inner join brand on product.id_brand = brand.id_brand inner join category on product.id_category = category.id_cat;"
 );
--- web/views/products.php
+++ web/views/products.php
@@ -4,16 +4,17 @@
         require_once __DIR__ . '/../models/Database.php';
 
         $prods = (new Database())->get_products();
+        $aws_link = getenv("AWS_LINK");
 
         foreach ($prods as $prod) {
-            echo '<div class="prod">';
-            echo '<img class="img-responsive" src="./images/gts3m.jpg" style="width: 100%;"/>';
-            echo '<h2>' . $prod['model'] . '</h2>';
-            echo '<h3>Marca: ' . $prod['brand'] . '</h3>';
-            echo '<h4>R$' . number_format($prod['price'], 2, ',', '.') . '</h4>';
-            echo '<p>' . $prod['category'] . '</p>';
-            echo '<p>' . $prod['description'] . '</p>';
-            echo '</div>';
+            echo
+            '<div class="prod">
+                <img style="border: 2px solid #e5e5e5; padding: 0.5em; width: 300px; height: 300px; margin:auto; display: block; margin-bottom: 1em; object-fit: cover;" src="' . $aws_link . $prod['image'] . '"/>
+                <h2>' . $prod['model'] . '</h2>
+                <p>' . $prod['category'] . '</p>
+                <h3>Marca: ' . $prod['brand'] . '</h3>
+                <h4>R$' . number_format($prod['price'], 2, ',', '.') . '</h4>
+            </div>';
         }
         ?>
     </div>
